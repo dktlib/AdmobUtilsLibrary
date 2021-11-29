fd62129c6f9a2ad213cceb3e064da11a580e5f83
namdh12 <namdh1212@gmail.com>
2021-11-29 13:41:52 +0700

    Add Adrequest Null check

--- admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AdmodUtils.java
+++ admoblibrary/src/main/java/com/vapp/admoblibrary/ads/AdmodUtils.java
@@ -230,7 +230,9 @@ public class AdmodUtils {
             }
         });
 
-        mAdView.loadAd(adRequest);
+        if(adRequest!=null){
+            mAdView.loadAd(adRequest);
+        }
         Log.e(" Admod", "loadAdBanner");
     }
 
@@ -273,7 +275,9 @@ public class AdmodUtils {
                 })
                 .withNativeAdOptions(new NativeAdOptions.Builder().build()).build();
 
-        adLoader.loadAd(adRequest);
+        if(adRequest!=null){
+            adLoader.loadAd(adRequest);
+        }
         Log.e("Admod", "loadAdNativeAds");
     }
 
@@ -325,7 +329,9 @@ public class AdmodUtils {
                 })
                 .withNativeAdOptions(new NativeAdOptions.Builder().build()).build();
 
-        adLoader.loadAd(adRequest);
+        if(adRequest!=null){
+            adLoader.loadAd(adRequest);
+        }
         Log.e("Admod", "loadAdNativeAds");
     }
 
